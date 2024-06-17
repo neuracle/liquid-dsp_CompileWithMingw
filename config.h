c69736913b2ee5caf5cb9da28212e00edf031c04
gongch <gongch@neuracle.cn>
2024-06-17 15:38:32 +0800

    Add config.h & makefile.mingw**

---
+++ config.h
@@ -0,0 +1,290 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+
+#ifndef __LIQUID_CONFIG_H__
+#define __LIQUID_CONFIG_H__
+
+
+/* Define to 1 to support Advanced Bit Manipulation */
+#define HAVE_ABM 1
+
+/* Define to 1 to support Multi-Precision Add-Carry Instruction Extensions */
+#define HAVE_ADX 1
+
+/* Define to 1 to support Advanced Encryption Standard New Instruction Set
+   (AES-NI) */
+/* #define HAVE_AES 1 */
+
+/* Support Altivec instructions */
+/* #undef HAVE_ALTIVEC */
+
+/* Define to 1 to support Advanced Vector Extensions */
+/* #define HAVE_AVX 1 */
+
+/* Define to 1 to support Advanced Vector Extensions 2 */
+/* #define HAVE_AVX2 1 */
+
+/* Define to 1 to support AVX-512 Byte and Word Instructions */
+/* #undef HAVE_AVX512_BW */
+
+/* Define to 1 to support AVX-512 Conflict Detection Instructions */
+/* #undef HAVE_AVX512_CD */
+
+/* Define to 1 to support AVX-512 Doubleword and Quadword Instructions */
+/* #undef HAVE_AVX512_DQ */
+
+/* Define to 1 to support AVX-512 Exponential & Reciprocal Instructions */
+/* #undef HAVE_AVX512_ER */
+
+/* Define to 1 to support AVX-512 Foundation Extensions */
+/* #undef HAVE_AVX512_F */
+
+/* Define to 1 to support AVX-512 Integer Fused Multiply Add Instructions */
+/* #undef HAVE_AVX512_IFMA */
+
+/* Define to 1 to support AVX-512 Conflict Prefetch Instructions */
+/* #undef HAVE_AVX512_PF */
+
+/* Define to 1 to support AVX-512 Vector Byte Manipulation Instructions */
+/* #undef HAVE_AVX512_VBMI */
+
+/* Define to 1 to support AVX-512 Vector Length Extensions */
+/* #undef HAVE_AVX512_VL */
+
+/* Define to 1 to support Bit Manipulation Instruction Set 1 */
+#define HAVE_BMI1 1
+
+/* Define to 1 to support Bit Manipulation Instruction Set 2 */
+#define HAVE_BMI2 1
+
+/* Define to 1 if you have the `cargf' function. */
+#define HAVE_CARGF 1
+
+/* Define to 1 if you have the `cexpf' function. */
+#define HAVE_CEXPF 1
+
+/* Define to 1 if you have the `cimagf' function. */
+#define HAVE_CIMAGF 1
+
+/* Define to 1 if you have the <complex.h> header file. */
+#define HAVE_COMPLEX_H 1
+
+/* Define to 1 if you have the `cosf' function. */
+#define HAVE_COSF 1
+
+/* Define to 1 if you have the `crealf' function. */
+#define HAVE_CREALF 1
+
+/* Define to 1 if you have the `expf' function. */
+#define HAVE_EXPF 1
+
+/* Define to 1 if you have the <fec.h> header file. */
+/* #undef HAVE_FEC_H */
+
+/* Define to 1 if you have the <fftw3.h> header file. */
+/* #undef HAVE_FFTW3_H */
+
+/* Define to 1 if you have the <float.h> header file. */
+#define HAVE_FLOAT_H 1
+
+/* Define to 1 to support Fused Multiply-Add Extensions 3 */
+#define HAVE_FMA3 1
+
+/* Define to 1 to support Fused Multiply-Add Extensions 4 */
+/* #undef HAVE_FMA4 */
+
+/* Define to 1 if you have the `free' function. */
+#define HAVE_FREE 1
+
+/* Define to 1 if you have the <getopt.h> header file. */
+#define HAVE_GETOPT_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `c' library (-lc). */
+#define HAVE_LIBC 1
+
+/* Define to 1 if you have the `fec' library (-lfec). */
+/* #undef HAVE_LIBFEC */
+
+/* Define to 1 if you have the `fftw3f' library (-lfftw3f). */
+/* #undef HAVE_LIBFFTW3F */
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if you have the <limits.h> header file. */
+#define HAVE_LIMITS_H 1
+
+/* Define to 1 if you have the `malloc' function. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the `memmove' function. */
+#define HAVE_MEMMOVE 1
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 to support Multimedia Extensions */
+#define HAVE_MMX 1
+
+/* Define to 1 to support Memory Protection Extensions */
+/* #undef HAVE_MPX */
+
+/* Define to 1 to support Prefetch Vector Data Into Caches WT1 */
+/* #undef HAVE_PREFETCHWT1 */
+
+/* Define to 1 to support Digital Random Number Generator */
+#define HAVE_RDRND 1
+
+/* Define to 1 if you have the `realloc' function. */
+#define HAVE_REALLOC 1
+
+/* Define to 1 to support Secure Hash Algorithm Extension */
+/* #undef HAVE_SHA */
+
+/* Define to 1 if you have the `sinf' function. */
+#define HAVE_SINF 1
+
+/* Define to 1 if you have the `sqrtf' function. */
+#define HAVE_SQRTF 1
+
+/* Define to 1 to support Streaming SIMD Extensions */
+#define HAVE_SSE 1
+
+/* Define to 1 to support Streaming SIMD Extensions */
+#define HAVE_SSE2 1
+
+/* Define to 1 to support Streaming SIMD Extensions 3 */
+#define HAVE_SSE3 1
+
+/* Define to 1 to support Streaming SIMD Extensions 4.1 */
+#define HAVE_SSE4_1 1
+
+/* Define to 1 to support Streaming SIMD Extensions 4.2 */
+#define HAVE_SSE4_2 1
+
+/* Define to 1 to support AMD Streaming SIMD Extensions 4a */
+/* #undef HAVE_SSE4a */
+
+/* Define to 1 to support Supplemental Streaming SIMD Extensions 3 */
+#define HAVE_SSSE3 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/resource.h> header file. */
+#define HAVE_SYS_RESOURCE_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Support VSX instructions */
+/* #undef HAVE_VSX */
+
+/* Define to 1 to support eXtended Operations Extensions */
+/* #undef HAVE_XOP */
+
+/* Force internal FFT even if libfftw is available */
+#undef LIQUID_FFTOVERRIDE
+
+/* Force overriding of SIMD (use portable C code) */
+/* #undef LIQUID_SIMDOVERRIDE */
+
+/* Enable strict program exit on error */
+/* #undef LIQUID_STRICT_EXIT */
+
+/* Suppress printing errors to stderr */
+/* #undef LIQUID_SUPPRESS_ERROR_OUTPUT */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "joseph@liquidsdr.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "liquid-dsp"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "liquid-dsp 1.5"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "liquid-dsp"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.5"
+
+/* The size of `int', as computed by sizeof. */
+#define SIZEOF_INT 4
+
+/* The size of `long int', as computed by sizeof. */
+#define SIZEOF_LONG_INT 8
+
+/* The size of `long long int', as computed by sizeof. */
+#define SIZEOF_LONG_LONG_INT 8
+
+/* The size of `short int', as computed by sizeof. */
+#define SIZEOF_SHORT_INT 2
+
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
+#define STDC_HEADERS 1
+
+/* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT32_T */
+
+/* Define for Solaris 2.5.1 so the uint8_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT8_T */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
+
+/* Define to rpl_realloc if the replacement function should be used. */
+/* #undef realloc */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to the type of an unsigned integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint32_t */
+
+/* Define to the type of an unsigned integer type of width exactly 8 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint8_t */
+
+
+#endif // __LIQUID_CONFIG_H__
+
